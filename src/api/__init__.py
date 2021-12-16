3c4f8d7636ad93edec6ef4fdad2f003249f47a3e
2_taehyun <63915557+0417taehyun@users.noreply.github.com>
2021-12-16 16:27:16 +0900

    :construction: Initialize develop settings such as pytest, pre-commit

---
+++ src/api/__init__.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+
+from src.api import question
+
+
+router = APIRouter()
+
+router.include_router(
+    router = question.router,
+    prefix = '/question',
+    tags = ['questions']
+)
