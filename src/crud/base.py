88a2fa0571dd0d8030b87bd1326e911ebe3f06ce
2_taehyun <63915557+0417taehyun@users.noreply.github.com>
2021-12-16 16:23:56 +0900

    :recylce: Combine Get Pagination and Get All in CRUDBase

--- src/crud/base.py
+++ src/crud/base.py
@@ -1,8 +1,9 @@
-from typing import TypeVar, Generic, Optional
-from bson.objectid import ObjectId
+from typing import TypeVar, Generic, Optional, List
 from fastapi import Request
 from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
+from pymongo import ASCENDING, DESCENDING
+from bson.objectid import ObjectId
 
 CreateSchema = TypeVar('CreateSchema', bound=BaseModel)
 UpdateSchema =  TypeVar('UpdateSchema', bound=BaseModel)
@@ -18,31 +19,54 @@ class CRUDBase(Generic[CreateSchema, UpdateSchema]):
         
         return document
         
-    async def get_all(self, request: Request) -> list:        
-        documents = await request.app.db[
-            self.collection
-        ].find().to_list(length=None)
+    async def get_multi(
+        self,
+        skip: Optional[int],
+        limit: Optional[int],
+        request: Request,
+        sort: Optional[List[str]]
+    ) -> list:
+        query = request.app.db[self.collection].find()
+        
+        if sort:
+            sort_field = []
+            
+            if type(sort) is list:
+                for query_string in sort:
+                    field, order = query_string.split(' ')
+                    
+                    field = field.replace('-', '_')
+                    
+                    if order == 'asc':
+                        order = ASCENDING
+                    elif order == 'desc':
+                        order = DESCENDING
+                    else:
+                        raise ValueError
+                    
+                    sort_field.append((field, order))
+        
+            else:
+                field, order = sort.split(' ')
+        
+                field = field.replace('-', '_')
+                
+                if order == 'asc':
+                    order = ASCENDING
+                elif order == 'desc':
+                    order = DESCENDING
+                else:
+                    raise ValueError
+                
+                sort_field.append((field, order))
+        
+            query = query.sort(sort_field)
+        
+        documents = await query.skip(skip).limit(limit).to_list(length=None)
         
         for document in documents:
             document['_id'] = str(document['_id'])
 
-        return documents
-    
-    async def get_multi(
-        self,
-        request: Request,
-        limit: int,
-        skip: int
-    ) -> list:
-        documents = await request.app.db[self.collection].find().skip(
-            skip
-        ).limit(
-            limit
-        ).to_list(length=None)
-        
-        for document in documents:
-            document['_id'] = str(document['_id'])
-        
         return documents
         
     async def create(
